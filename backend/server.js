6ecc893199f034676af7390024cee854ca81729e
radyacharib-cpu <radya.charib@etu.uae.ac.ma>
2026-05-19 19:37:19 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ app.use("/api/tasks", require("./routes/tasks"));
 app.use("/api/dashboard", require("./routes/dashboard"));
 
 mongoose.connect("mongodb://mongo:27017/taskflow")
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.log("❌ MongoDB error:", err));
+  .then(() => console.log(" MongoDB connected"))
+  .catch(err => console.log(" MongoDB error:", err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
